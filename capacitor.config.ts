fbd4faef9dbe35cd0c41a5c68f501abc9b9bd8e8
Adriane Justine Tan <eidoriantan@gmail.com>
2024-10-08 23:29:51 +0800

    Change bundle ID

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.alanchen.sightsky',
+  appId: 'me.eidoriantan.sightsky',
   appName: 'SightSky',
   webDir: 'dist'
 };
